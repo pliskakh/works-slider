90500d100253265af638f21ba9da914636d3f8b8
Mykhailo Pliskunov <pliskunov.mykhailo@gmail.com>
2017-12-19 23:09:46 +0200

    slick slider added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-;(function(){
+;(function($){
 	"use strict";
+
+	var slider = $('.ba-slider');
+	slider.slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		arrows: true,
+		slide: '.ba-slide',
+		prevArrow: '.ba-slide__controls--prev',
+		nextArrow: '.ba-slide__controls--next'
+	});
 	
-})();
+})(jQuery);
